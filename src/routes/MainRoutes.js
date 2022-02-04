caad9236642bb3fdfd9ebb96d16f5dd0479a1a0f
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2022-02-03 20:26:44 -0600

    added social profile and use list

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -9,7 +9,10 @@ import Calendar from 'views/application/calendar';
 // sample page routing
 const SamplePage = Loadable(lazy(() => import('views/sample-page')));
 const AppChat = Loadable(lazy(() => import('views/application/chat')));
-const AppCalendar = Loadable(lazy(() => import('views/application/calendar')))
+const AppCalendar = Loadable(lazy(() => import('views/application/calendar')));
+const AppUserSocialProfile = Loadable(lazy(() => import('views/application/users/social-profile')));
+const AppUserAccountProfile1 = Loadable(lazy(() => import('views/application/users/account-profile/Profile1')));
+const AppProfileCardStyle1 = Loadable(lazy(() => import('views/application/users/card/CardStyle1')));
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
@@ -27,6 +30,18 @@ const MainRoutes = {
         {
             path: '/app/chat',
             element: <AppChat />
+        },
+        {
+            path: '/user/social-profile/:tab',
+            element: <AppUserSocialProfile />
+        },
+        {
+            path: '/user/account-profile/profile1',
+            element: <AppUserAccountProfile1 />
+        },
+        {
+            path: '/user/list',
+            element: <AppProfileCardStyle1 />
         },
         {
             path: '/app/calendar',
